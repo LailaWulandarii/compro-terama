a1b8b9c28806dc1a6b29f5bf6c7be208938d0f4b
LailaWulandarii <E41220418@student.polije.ac.id>
2025-10-07 10:21:25 +0700

    add header and page description for about section

--- app/Views/about.php
+++ app/Views/about.php
@@ -4,11 +4,12 @@
 <!-- ======= Breadcrumbs ======= -->
 <div class="breadcrumbs d-flex align-items-center">
   <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
-
-    <h2><?= $lang == 'id' ? 'Tentang Kami' : 'About Us'; ?></h2>
-
+    <h2> <?= $lang === 'id' ? $meta['nama_halaman_id'] : $meta['nama_halaman_en']; ?></h2>
+    <p class="mt-2" style="color: white;">
+      <?= $lang == 'id' ? $meta['deskripsi_halaman_id'] : $meta['deskripsi_halaman_en']; ?>
+    </p>
   </div>
-</div><!-- End Breadcrumbs -->
+</div>
 <section id="get-started" class="get-started features section-bg">
   <div class="container">
     <div class="tab-pane" id="tab-2">
