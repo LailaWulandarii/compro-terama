2a3321e3e69ea782c1806b5acbdf9a13cc6f16ad
LailaWulandarii <E41220418@student.polije.ac.id>
2025-10-08 10:32:41 +0700

    refactor all code,modify hard code to dynamic with database for activity page

--- app/Views/activity.php
+++ app/Views/activity.php
@@ -15,198 +15,56 @@
   <!-- ======= Our Projects Section ======= -->
   <section id="projects" class="projects">
     <div class="container" data-aos="fade-up">
+      <div class="row gy-4">
 
-      <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry"
-        data-portfolio-sort="original-order">
+        <!-- Filter kategori aktivitas -->
 
-        <ul class="portfolio-flters" data-aos="fade-up" data-aos-delay="100">
-          <li data-filter="*" class="filter-active">All</li>
-          <li data-filter=".filter-remodeling">Remodeling</li>
-          <li data-filter=".filter-construction">Construction</li>
-          <li data-filter=".filter-repairs">Repairs</li>
-          <li data-filter=".filter-design">Design</li>
-        </ul><!-- End Projects Filters -->
-
-        <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/remodeling-1.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Remodeling 1</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/remodeling-1.jpg" title="Remodeling 1"
-                  data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
-                    class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
+        <div class="col-12 mb-4 text-center">
+          <?php foreach ($categoriesAktivitas as $cat): ?>
+            <?php
+            $slug = $lang === 'id' ? $cat['slug_kategori_id'] : $cat['slug_kategori_en'];
+            $nama = $lang === 'id' ? $cat['nama_kategori_id'] : $cat['nama_kategori_en'];
+            ?>
+            <a href="<?= base_url($lang . '/' . ($lang === 'id' ? 'aktivitas' : 'activity') . '/' . $slug) ?>"
+              class="btn btn-outline-primary btn-sm m-1">
+              <?= esc($nama) ?>
+            </a>
+          <?php endforeach; ?>
+        </div>
+        <?php if (!empty($allAktivitas)): ?>
+          <?php foreach ($allAktivitas as $item): ?>
+            <div class="col-lg-4 col-md-6 col-12 mb-4">
+              <div class="card h-100">
+                <img src="<?= base_url('assets/img/aktivitas/' . esc($item['foto_aktivitas'])) ?>"
+                  class="card-img-top"
+                  alt="<?= esc($lang === 'id' ? $item['alt_aktivitas_id'] : $item['alt_aktivitas_en']) ?>">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    <?= esc($lang === 'id' ? $item['judul_aktivitas_id'] : $item['judul_aktivitas_en']) ?>
+                  </h5>
+                  <p class="card-text"><small class="text-muted"><span class="badge text-bg-primary"><?= $lang == 'id' ? $item['nama_kategori'] : $item['nama_kategori']; ?></span> - <?= date('d M Y', strtotime($item['updated_at'])); ?>
+                    </small></p>
+                  <p class="card-text">
+                    <?= $lang === 'id' ? $item['meta_desc_id'] : $item['meta_desc_en'] ?>...
+                  </p>
+                  <?php
+                  $categorySlug = $lang === 'id' ? $item['slug_kategori_id'] : $item['slug_kategori_en'];
+                  $activitySlug = $lang === 'id' ? $item['slug_aktivitas_id'] : $item['slug_aktivitas_en'];
+                  $urlmenu = $lang === 'id' ? 'aktivitas' : 'activity';
+                  ?>
+                  <a href="<?= base_url("$lang/$urlmenu/$categorySlug/$activitySlug") ?>" class="custom-btn btn ">
+                    <?= $lang === 'id' ? 'Lihat Detail' : 'View Detail' ?>
+                  </a>
+                </div>
               </div>
             </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-construction">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/construction-1.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Construction 1</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/construction-1.jpg" title="Construction 1"
-                  data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i
-                    class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/repairs-1.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Repairs 1</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/repairs-1.jpg" title="Repairs 1" data-gallery="portfolio-gallery-repairs"
-                  class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-design">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/design-1.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Design 1</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/design-1.jpg" title="Repairs 1" data-gallery="portfolio-gallery-book"
-                  class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/remodeling-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Remodeling 2</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/remodeling-2.jpg" title="Remodeling 2"
-                  data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
-                    class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-construction">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/construction-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Construction 2</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/construction-2.jpg" title="Construction 2"
-                  data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i
-                    class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/repairs-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Repairs 2</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/repairs-2.jpg" title="Repairs 2" data-gallery="portfolio-gallery-repairs"
-                  class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-design">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/design-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Design 2</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/design-2.jpg" title="Repairs 2" data-gallery="portfolio-gallery-book"
-                  class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/remodeling-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Remodeling 3</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/remodeling-3.jpg" title="Remodeling 3"
-                  data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
-                    class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-construction">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/construction-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Construction 3</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/construction-3.jpg" title="Construction 3"
-                  data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i
-                    class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/repairs-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Repairs 3</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/repairs-3.jpg" title="Repairs 2" data-gallery="portfolio-gallery-repairs"
-                  class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-          <div class="col-lg-4 col-md-6 portfolio-item filter-design">
-            <div class="portfolio-content h-100">
-              <img src="assets/img/projects/design-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Design 3</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur</p>
-                <a href="assets/img/projects/design-3.jpg" title="Repairs 3" data-gallery="portfolio-gallery-book"
-                  class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                <a href="project-details.html" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
-              </div>
-            </div>
-          </div><!-- End Projects Item -->
-
-        </div><!-- End Projects Container -->
-
+          <?php endforeach; ?>
+        <?php else: ?>
+          <div class="col-12 text-center">
+            <p><?= $lang === 'id' ? 'Belum ada aktivitas.' : 'No activities available.' ?></p>
+          </div>
+        <?php endif; ?>
       </div>
-
     </div>
   </section><!-- End Our Projects Section -->
 
