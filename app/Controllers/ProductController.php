08e9a38821aef29d09e31b4815741ea7d8998446
LailaWulandarii <E41220418@student.polije.ac.id>
2025-10-08 10:31:52 +0700

    delete canonical code in product controller

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -137,9 +137,6 @@ class ProductController extends BaseController
 
         $canonical = base_url("$lang/" . ($lang === 'id' ? 'produk' : 'product') . '/' . $slugCheck);
 
-        if (current_url() !== $canonical) {
-            return redirect()->to($canonical);
-        }
 
         // Siapkan data untuk ditampilkan ke view
         $data = [
