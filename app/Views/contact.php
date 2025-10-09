84558fd57a99ee84096201e7020c64c83f5bff87
LailaWulandarii <E41220418@student.polije.ac.id>
2025-10-09 09:13:41 +0700

    refactor contact view

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -10,21 +10,40 @@
     </p>
   </div>
 </div>
+<section id="contact" class="contact-section ">
 
-<section id="contact" class="contact-section mb-5 section-bg" style="margin-top: 30px;">
   <div class="container">
-    <div class="row gy-4">
-      <div class="aboutcard col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-        <div class="responsive-map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.525694167994!2d112.61282937401006!3d-7.9445006920797265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78827489ab9d67%3A0xcad03ec85e51098!2sJl.%20Simpang%20Remujung%20No.3%2C%20Jatimulyo%2C%20Kec.%20Lowokwaru%2C%20Kota%20Malang%2C%20Jawa%20Timur%2065141!5e0!3m2!1sid!2sid!4v1739112900065!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="section-header mb-2">
+      <h2><?= $lang == 'id' ? $meta['nama_halaman_id'] : $meta['nama_halaman_en']; ?></h2>
+      <p><?= $lang == 'id' ? $meta['title_id'] : $meta['title_en']; ?></p>
+    </div>
+    <div class="row align-items-center">
+      <!-- Kolom kiri: informasi kontak dari database -->
+      <div class="col-lg-4 col-12 ms-auto mb-5 mb-lg-0">
+        <div class="contact-info-wrap">
+          <!-- <h2><?= esc($lang == 'id' ? $meta['nama_halaman_id'] : $meta['nama_halaman_en']); ?></h2> -->
+
+
+          <div class="contact-info">
+            <!-- <h5 class="mb-3"><?= ($lang == 'id') ? $meta['deskripsi_halaman_id'] : $meta['deskripsi_halaman_en'] ?></h5> -->
+
+            <p><?= ($lang == 'id') ? $kontak['deskripsi_kontak_id'] : $kontak['deskripsi_kontak_en'] ?></p>
+          </div>
         </div>
       </div>
-      <div class="col-lg-6 order-1 order-lg-1 hero-img">
-        <p class="deskripsi">
-          <?= $lang == 'id' ? $kontak['deskripsi_kontak_id'] : $kontak['deskripsi_kontak_en']; ?>
-        </p>
-        <div class="d-flex">
-          <a href="<?= base_url($lang == 'id' ? 'id/kontak' : 'en/contact') ?>" class="button-text button-bg"><?= lang('bahasa.Baca Selengkapnya'); ?></a>
+
+      <!-- Kolom kanan: Google Maps -->
+      <div class="col-lg-6 col-12 mx-auto">
+        <div class="ratio ratio-4x3">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.525694167994!2d112.61282937401006!3d-7.9445006920797265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78827489ab9d67%3A0xcad03ec85e51098!2sJl.%20Simpang%20Remujung%20No.3%2C%20Jatimulyo%2C%20Kec.%20Lowokwaru%2C%20Kota%20Malang%2C%20Jawa%20Timur%2065141!5e0!3m2!1sid!2sid!4v1739112900065!5m2!1sid!2sid"
+            width="100%"
+            height="100%"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+          </iframe>
         </div>
       </div>
     </div>
