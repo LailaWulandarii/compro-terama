65c4b0d200227c48774307c1c108abff5cdee6d0
LailaWulandarii <E41220418@student.polije.ac.id>
2025-10-12 09:29:42 +0700

    delete old code

--- app/Views/detail_product.php
+++ app/Views/detail_product.php
@@ -11,47 +11,7 @@
     </p>
   </div>
 </div>
-<div class="container bg-white p-4 rounded shadow-sm">
-  <div class="row">
-    <!-- Gambar Produk -->
-    <div class="col-lg-6 col-md-6 col-12 mb-4 ">
-      <img src="<?= base_url('assets/img/produk/' . $product['foto_produk']); ?>"
-        class="img-fluid rounded shadow"
-        alt="<?= esc($lang === 'id' ? $product['alt_produk_id'] : $product['alt_produk_en']); ?>">
-    </div>
 
-    <!-- Deskripsi Produk -->
-    <div class="col-lg-6 col-md-6 col-12">
-      <h2 class="mb-3">
-        <?= esc($lang === 'id' ? $product['nama_produk_id'] : $product['nama_produk_en']); ?>
-      </h2>
-
-      <p>
-        <?= $lang == 'id' ? $product['deskripsi_produk_id'] : $product['deskripsi_produk_en']; ?>
-      </p>
-
-      <!-- Jika butuh tombol aksi -->
-      <a href="<?= base_url($lang . '/produk'); ?>" class="btn btn-outline-primary mt-3">
-        <?= $lang === 'id' ? 'Kembali ke Daftar Produk' : 'Back to Product List'; ?>
-      </a>
-    </div>
-  </div>
-</div>
 <section class="product-detail">
-  <div class="container">
-    <div class="row gy-4 align-items-start">
-      <div class="col-lg-6 order-2 order-lg-1">
-        <div class="image-wrapper">
-          <img src="<?= base_url('assets/img/produk/' . $product["foto_produk"]) ?>"
-            alt="<?= $lang == 'id' ? $product['alt_produk_id'] : $product['alt_produk_en']; ?>" class="product-image">
-        </div>
-      </div>
-
-      <div class="col-lg-6 order-1 order-lg-2">
-        <h1 class="product-title"><?= $lang == 'id' ? $product['nama_produk_id'] : $product['nama_produk_en']; ?></h1>
-        <?= $lang == 'id' ? $product['deskripsi_produk_id'] : $product['deskripsi_produk_en']; ?>
-      </div>
-    </div>
-  </div>
 </section>
 <?= $this->endSection(); ?>
